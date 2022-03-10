f2f996962bfc698199e5e827ee286b6b02be9d14
Kirill <info@sampx.ru>
2022-03-10 16:01:19 +0300

    Лекция №2. Знакомство с Java

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -1,40 +1,52 @@
 package core;
+
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Game {
     final char SIGN_X = 'x';
     final char SIGN_O = 'o';
     char table[][] = new char[3][3];
 
+    public static boolean rsl = true;
+
     public void game() throws IOException {
-        String playerWin = "Player Win!";
-        String aiWin = "AI WIN";
+        String playerNameOne = Turn.getName("Player One");
+        System.out.println("Если хотите сыграть с роботом напишите AI" +
+                "\nЕсли хотите остановить игру введите Stop");
+        String playerNameTwo = Turn.getName("Player Two");
+        String win = " - Win!!!";
         table = InAndOutTable.initTable(table);
-        while (true) {
-           Turn.turnHuman(table);
+        do {
+            Turn.turnHuman(table, 'x');
             if (Cheks.checkWin(SIGN_X, table)) {
-                Write.write(playerWin);
-                System.out.println(playerWin);
-                break;
+                Write.write(playerNameOne + win);
+                table = InAndOutTable.initTable(table);
+                System.out.println(playerNameOne + win);
+                continue;
             }
             if (Cheks.isTableFull(table)) {
                 System.out.println("Sorry, DRAW!");
-                break;
+                table = InAndOutTable.initTable(table);
+                continue;
             }
-            Turn.turnAI(table);
+            Turn.turnAI(table, playerNameTwo);
             InAndOutTable.printTable(table);
             if (Cheks.checkWin(SIGN_O, table)) {
-                Write.write(aiWin);
-                System.out.println(aiWin);
-                break;
+                Write.write(playerNameTwo + win);
+                table = InAndOutTable.initTable(table);
+                System.out.println(playerNameTwo + win);
+                continue;
             }
             if (Cheks.isTableFull(table)) {
                 System.out.println("Sorry, DRAW!");
-                break;
+                continue;
             }
-        }
-        InAndOutTable.printTable(table);
+        } while (isProceed());
     }
 
-
+    public static boolean isProceed() {
+        boolean rsl = Game.rsl;
+        return rsl;
+    }
 }
--- src/main/java/core/Turn.java
+++ src/main/java/core/Turn.java
@@ -4,24 +4,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Turn {
-   public static void turnHuman(char[][] table) {
+    static Scanner scanner = new Scanner(System.in);
+    public static void turnHuman(char[][] table, char symbol) {
         int x, y;
-        Scanner scanner = new Scanner(System.in);
         do {
             System.out.println("Enter X and Y (1..3):");
             x = scanner.nextInt() - 1;
             y = scanner.nextInt() - 1;
+            if (x < 0 || y < 0) {
+                Game.rsl = false;
+                break;
+            }
         } while (!Cheks.isCellValid(table, x, y));
-        table[y][x] = 'x';
+        table[y][x] = symbol;
     }
 
-  public static void turnAI(char[][] table) {
+    public static void turnAI(char[][] table, String playerNameTwo) {
         int x, y;
-        Random random = new Random();
-        do {
-            x = random.nextInt(3);
-            y = random.nextInt(3);
-        } while (!Cheks.isCellValid(table, x, y));
-        table[y][x] = 'o';
+        if (playerNameTwo.equalsIgnoreCase("AI")) {
+            Random random = new Random();
+            do {
+                x = random.nextInt(3);
+                y = random.nextInt(3);
+            } while (!Cheks.isCellValid(table, x, y));
+            table[y][x] = 'o';
+        }else {
+            turnHuman(table, 'o');
+        }
+    }
+
+    public static String getName(String player) {
+        System.out.println("Введите имя: " + player);
+        return scanner.nextLine();
+
     }
 }
